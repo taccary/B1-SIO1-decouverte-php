ac27111749b8ecd2d12f8cc4cd7dca38c6d6f1da
taccary <tiphaine.accary-barbier@icof.fr>
2020-11-20 17:45:31 +0100

    maj fetch_assoc

--- pages/jeux/afficheUnJeu.php
+++ pages/jeux/afficheUnJeu.php
@@ -8,11 +8,11 @@
 	//var_dump($jeu); // on affiche le contenu de la variable $jeu (ici un tableau php array())
 	$stmt->closeCursor(); // on ferme le curseur des résultats*/
 
-	
-	$SQL = ""; // ecrire la requete qui va chercher les commentaires du jeu affiché
+	//$SQL = "SELECT * FROM avis JOIN membre ON avis.idMembre = membre.idMembre WHERE idJeu = ? ";
+	$SQL = "SELECT * FROM avis NATURAL JOIN membre WHERE idJeu = ? "; // ecrire la requete qui va chercher les commentaires du jeu affiché
 	$stmt = $connexion->prepare($SQL);
 	$stmt->execute(array($idJeu)); // on passe dans le tableaux les paramètres si il y en a à fournir (ici l'identifiant du jeu)
-	$commentaires = $stmt->fetchall(); // on met le resultat de la requete dans un tableau
+	$commentaires = $stmt->fetchall(PDO::FETCH_ASSOC); // on met le resultat de la requete dans un tableau
 	//var_dump($commentaires); // on affiche le contenu de la variable $jeu (ici un tableau php array())
 	$stmt->closeCursor(); // on ferme le curseur des résultats*/
 
@@ -28,7 +28,14 @@
 		<tr>
 			<td rowspan="2" class="photo">
 				<!-- si le jeu n'a pas d'image, afficher "Aucune image disponible pour ce jeu" -->
-				<img src="pages/jeux/photos/<?= $jeu['photo'] ?>" alt="image du jeu <?= $jeu['nom'] ?>"/>
+				<?php if($jeu['photo'] == null){
+					echo "Aucune image disponible pour ce jeu";
+				} else {
+					?>
+					<img src="pages/jeux/photos/<?= $jeu['photo'] ?>" alt="image du jeu <?= $jeu['nom'] ?>"/>
+				<?php
+				}
+				?>
 			</td>
 			<td colspan="2">
 				<?= $jeu['console'] ?> <!-- afficher le nom de la console et pas son numéro -->
@@ -41,6 +48,40 @@
 	</tbody>
 </table>
 
-<h4>Commentaires de ce jeu (xx<!-- ajouter le code pour afficher le nombre de commentaires pour ce jeu en utilisant la fonction count() du langage php sur le tableau $commentaires (voir sa documentation sur internet) -->)	</h4>
+<?php if(count($commentaires) != 0){
+	?>
+	<h4>Commentaires de ce jeu (<?php echo count($commentaires)?>)<!-- ajouter le code pour afficher le nombre de commentaires pour ce jeu en utilisant la fonction count() du langage php sur le tableau $commentaires (voir sa documentation sur internet) -->)	</h4>
 
-<!-- ajouter le code pour afficher les commentaires pour ce jeu sous forme d'une liste à puces alimentées par une boucle -->	
+	<!-- ajouter le code pour afficher les commentaires pour ce jeu sous forme d'une liste à puces alimentées par une boucle -->	
+	<ul>
+	<?php
+	foreach ($commentaires as $unCommentaire){
+		echo "<li><strong>".$unCommentaire['nomMembre']."</strong>(".$unCommentaire['date'].") : ".$unCommentaire['commentaire']."</li>";
+	}
+	?>
+
+	<?php
+	/*version alternative
+	foreach ($commentaires as $commentaire){
+		?>
+		<li><strong><?=$commentaire['nomMembre']?></strong>(<?=$commentaire['date']?>) : <?=$commentaire['commentaire']?></li>"
+	<?php
+	}*/
+	?>
+	</ul>
+<?php
+} else {
+	echo "aucun avis pour l’instant ";
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
